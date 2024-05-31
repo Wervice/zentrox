1fd071bd7bfdf49d814700db3a4e8eb2a752b4ce
Wervice <wervice@proton.me>
2024-05-31 14:15:09 +0200

    Adding defaults to uninstaller

--- uninstall.bash
+++ uninstall.bash
@@ -16,23 +16,23 @@ echo "ℹ️ Assuming ~/zentrox and ~/zentrox_data to be the folders used by Zen
 
 echo -n "❓ Do you want to remove the Zentrox server? [y/N] "
 read
-if [[ $REPLY == "y" ]]; then
+if [[ $REPLY == "y" || $REPLY == "" ]]; then
 	rm -rf ~/zentrox/
 fi
 echo -n "❓ Do you want to remove the Zentrox data folder (includes user files and configuration)? [y/N] "
 read
-if [[ $REPLY == "y" ]]; then
+if [[ $REPLY == "y" || $REPLY == "" ]]; then
 	rm -rf ~/zentrox_data/
 fi
 echo -n "❓ Do you want to remove the Zentrox user (zentrox)? [y/N] "
 read
-if [[ $REPLY == "y" ]]; then
+if [[ $REPLY == "y" || $REPLY == "" ]]; then
 	sudo deluser zentrox &> /dev/null
 	sudo userdel zentrox --force &> /dev/null
 fi
 echo -n "❓ Do you want to remove the home folder of the Zentrox user (/home/zentrox) ? [y/N] "
 read
-if [[ $REPLY == "y" ]]; then
+if [[ $REPLY == "y" || $REPLY == "" ]]; then
 	sudo rm -rf /home/zentrox/
 fi
 echo "ℹ️ Removed Zentrox"
