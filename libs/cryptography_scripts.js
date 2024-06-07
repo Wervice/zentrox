6a1fab42fb919cfe9c5a59893ebed474841e08b0
Wervice <wervice@proton.me>
2024-06-07 14:22:21 +0200

    More interface adaptions

--- libs/cryptography_scripts.js
+++ libs/cryptography_scripts.js
@@ -2,10 +2,19 @@
 // Basically just a wrapper for OpenSSl
 
 const chpr = require("child_process");
+const fs = require("fs")
+const path = require("path")
+const os = require("os")
 
-function decryptAES(in_file, password) {
+function decryptAES(in_string, password) {
+	// Missing string handler
+	const zentroxInstPath = path.join(os.homedir(), "zentrox_data/");
+	let tmp_file = path.join(zentroxInstPath, btoa(Math.random()))
+	fs.writeFileSync(tmp_file, in_string)	
 	const child = chpr.execSync(
-		`openssl aes-256-cbc -d -a -pbkdf2 -in ${in_file} -pass pass:${password}`,
+		`openssl aes-256-cbc -d -a -pbkdf2 -in ${tmp_file} -pass pass:${password}`,
 	);
+	fs.writeFileSync(tmp_file, "----------------------------------------------------------------")
+	fs.unlinkSync(tmp_file)
 	return child.toString("ascii").replaceAll("\n", "");
 }
