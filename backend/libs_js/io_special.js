53fc621cace07a64f6a93257911b743fbaaa0072
Wervice <wervice@proton.me>
2024-09-19 07:02:37 +0200

    Removing libs_js

--- backend/libs_js/io_special.js
+++
@@ -1,7 +0,0 @@
-/*
-This library lets you manage file using sudo permissions.
-! This library is in no way secure, you can easily escape the command
-! Only use this on the admin front end with little to no user interaction
-*/
-
-function writeFileSync() {}
--- backend/libs_js/sudo.js
+++
@@ -1,9 +0,0 @@
-const chpr = require("child_process");
-
-function execSyncAsSudo(command, sudo) {
-	var execOut = chpr.execSync(
-		`echo "${sudo.replaceAll('"', '\\"').replaceAll("'", "\\'").replaceAll("$", "\\$")}" | -S ${command}`,
-		{ stdio: pipe },
-	);
-	return execOut;
-}
