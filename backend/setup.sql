00ade2357fffa39d0653b87c311a94fb74c4e40d
Wervice <wervice@proton.me>
2024-12-31 15:51:08 +0100

    Updated SQL schema
    Added MediaSources table and fix small issues

--- backend/setup.sql
+++ backend/setup.sql
@@ -1,7 +1,13 @@
 CREATE TABLE Secrets (
 	name varchar(255) NOT NULL, 
-	value varchar(1023), 
+	value varchar(5000), 
 	PRIMARY KEY (name)
+);
+CREATE TABLE MediaSources (
+	folderpath varchar(2047) NOT NULL,
+	alias varchar(255) NOT NULL,
+	enabled boolean NOT NULL,
+	PRIMARY KEY (folderpath)
 );
 CREATE TABLE Media (
 	filepath varchar(2047) NOT NULL, 
@@ -13,7 +19,8 @@ CREATE TABLE Media (
 );
 CREATE TABLE RecommendedMedia (
 	filepath varchar(2047) NOT NULL,
-	lastview date NOT NULL,
+	lastview integer NOT NULL,
+	category varchar(20) NOT NULL,
 	PRIMARY KEY	(filepath)
 );
 CREATE TABLE Ftp (
@@ -21,7 +28,6 @@ CREATE TABLE Ftp (
 	running boolean NOT NULL,
 	pid int,
 	username varchar(2047),
-	password varchar(2047),
 	local_root varchar(2047),
 	PRIMARY KEY	(key)
 );
@@ -34,5 +40,6 @@ CREATE TABLE Admin (
 	key int,
 	username varchar(1047),
 	use_otp boolean,
+	knows_otp boolean,
 	PRIMARY KEY (key)
 );
