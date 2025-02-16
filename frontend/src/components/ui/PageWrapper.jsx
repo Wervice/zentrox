575fb59933c06244c82971e14183fb9d7c8e7e72
Wervice <wervice@proton.me>
2025-02-16 21:06:00 +0100

    Added animation and new max height

--- frontend/src/components/ui/PageWrapper.jsx
+++ frontend/src/components/ui/PageWrapper.jsx
@@ -2,8 +2,12 @@ export default function Page({ name, children, className, ...props }) {
   return (
     <div
       className={
-        "w-full h-full flex-grow overflow-y-auto text-white " + className
-      }
+        "w-full h-full flex-grow overflow-y-auto text-white animate-fadein duration-300 overflow-hidden" + className
+      } style={
+		  {
+			  maxHeight: "calc(100vh - 76px)"
+		  }
+	  }
       {...props}
     >
       <div className="p-4 h-full">
