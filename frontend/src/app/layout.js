c83cb450ceecb41a5fe8694ec96e869087e9eaca
Wervice <wervice@proton.me>
2025-04-08 22:13:19 +0200

    Switched openSans to GeistSans

--- frontend/src/app/layout.js
+++ frontend/src/app/layout.js
@@ -1,10 +1,5 @@
-import { Open_Sans } from "next/font/google";
+import { GeistSans } from "geist/font/sans";
 import "./globals.css";
-
-const openSans = Open_Sans({
-  weight: ["400", "700", "800"],
-  subsets: ["latin"],
-});
 
 export const metadata = {
   title: "Zentrox",
@@ -15,9 +10,9 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <head>
-        <link rel="icon" type="image/x-icon" href="favicon.ico" />
+        <link rel="icon" type="image/x-icon" href="zentrox_dark_256.png" />
       </head>
-      <body className={openSans.className}>{children}</body>
+      <body className={GeistSans.className}>{children}</body>
     </html>
   );
 }
