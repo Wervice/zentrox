217c00e026d74f6a89b2923fe288dde8c24d412d
Wervice <wervice@proton.me>
2025-05-24 10:00:40 +0200

    Added a new way to handle uptime

---
+++ backend/src/uptime.rs
@@ -0,0 +1,30 @@
+use std::fs;
+use std::time::Duration;
+
+#[derive(Debug)]
+pub enum UptimeError {
+    ReadError,
+    BadData,
+    ParseError,
+}
+
+pub fn get() -> Result<Duration, UptimeError> {
+    let read = fs::read_to_string(std::path::Path::new("/proc/uptime"));
+    let d;
+    match read {
+        Ok(v) => d = v,
+        Err(_) => return Err(UptimeError::ReadError),
+    }
+
+    let seg: Vec<&str> = d.split(' ').collect();
+    if seg.len() < 2 {
+        return Err(UptimeError::BadData);
+    } else {
+        let v = seg[0];
+        let parsed = v.parse::<f32>();
+        match parsed {
+            Ok(v) => Ok(Duration::from_secs_f32(v)),
+            Err(_) => Err(UptimeError::ParseError),
+        }
+    }
+}
