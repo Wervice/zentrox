c0ea25be05c6f6cd6cb980b65d519039503bc56d
Wervice <wervice@proton.me>
2025-05-24 15:46:33 +0200

    Passing className value works

--- frontend/src/components/ui/InfoButton.jsx
+++ frontend/src/components/ui/InfoButton.jsx
@@ -11,11 +11,13 @@ import {
 } from "@/components/ui/dialog";
 import { Button } from "@/components/ui/button";
 
-export default function InfoButton({ title, info }) {
+export default function InfoButton({ title, info, className }) {
   return (
     <Dialog>
       <DialogTrigger asChild className="inline-block">
-        <InfoIcon className="h-4 w-4 inline-block text-neutral-600" />
+        <InfoIcon
+          className={"h-4 w-4 inline-block text-neutral-600 " + className}
+        />
       </DialogTrigger>
       <DialogContent>
         <DialogHeader>
