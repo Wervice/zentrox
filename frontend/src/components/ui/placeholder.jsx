b396b6e8557587d73ffa9aa52ca3ef93039406d9
Wervice <wervice@proton.me>
2025-05-24 19:47:31 +0200

    Added placeholder/splash screen component

---
+++ frontend/src/components/ui/placeholder.jsx
@@ -0,0 +1,21 @@
+function Placeholder({ children }) {
+  return <div className="w-full p-20">{children}</div>;
+}
+
+function PlaceholderIcon({ icon }) {
+  const Icon = icon;
+
+  return <>{<Icon className="w-full h-16 opacity-75 block mb-4" />}</>;
+}
+
+function PlaceholderSubtitle({ children }) {
+  return (
+    <>
+      <span className="block text-lg font-semibold w-full text-center opacity-75">
+        {children}
+      </span>
+    </>
+  );
+}
+
+export { PlaceholderSubtitle, Placeholder, PlaceholderIcon };
