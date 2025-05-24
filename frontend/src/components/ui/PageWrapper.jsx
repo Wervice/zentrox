7c8736dab157ecb602c46c91b1e957a4a5e1c64c
Wervice <wervice@proton.me>
2025-05-24 15:47:13 +0200

    Absolute title for splash screens

--- frontend/src/components/ui/PageWrapper.jsx
+++ frontend/src/components/ui/PageWrapper.jsx
@@ -1,17 +1,22 @@
-export default function Page({ name, children, className, ...props }) {
+export default function Page({
+  name,
+  children,
+  className,
+  titleAbsolute = false,
+  ...props
+}) {
   return (
     <div
       className={
-        "w-full h-full flex-grow overflow-y-auto text-white animate-fadein duration-300 overflow-hidden" +
+        "w-full h-screen flex-grow overflow-y-auto text-white animate-fadein duration-300 overflow-hidden" +
         className
       }
-      style={{
-        maxHeight: "calc(100vh - 76px)",
-      }}
       {...props}
     >
       <div className="p-4 h-full">
-        <h2 className="text-3xl font-bold">{name}</h2>
+        <h2 className={"text-3xl font-bold " + (titleAbsolute && "absolute")}>
+          {name}
+        </h2>
         {children}
       </div>
     </div>
