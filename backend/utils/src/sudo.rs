149d340b965abc4a976e3f366957e63282cb2207
Wervice <wervice@proton.me>
2025-12-10 20:24:09 +0100

    Removed spawn & code cleanup

--- backend/utils/src/sudo.rs
+++ backend/utils/src/sudo.rs
@@ -5,49 +5,46 @@ use std::process::Command;
 use std::process::Stdio;
 use std::thread;
 
-pub struct SwitchedUserCommand {
+#[derive(Debug)]
+pub struct SudoCommand {
     password: String,
     program: String,
     args: Vec<String>,
 }
 
-/// SudoOutput used with .output()
-#[allow(dead_code)]
 #[derive(Debug)]
-pub struct SudoOuput {
+pub struct SudoOutput {
     pub stdout: String,
     pub stderr: String,
+    pub status: Option<i32>,
 }
 
+/// Errors while running sudo
 #[derive(Debug)]
-pub enum SudoExecutionResult {
-    Success(i32),
+pub enum SudoError {
+    /// The supplied sudo password is invalid.
     WrongPassword,
-    Unauthorized,
-    ExecutionError(String),
+    /// Joining the process thread with the main thread failed.
+    JoiningFailed,
+    /// The user running Zentrox is not in the `sudoers` file.
+    NotInSudoers,
+    /// The supplied information may not be passed to sudo
+    BadParameters,
 }
 
-#[derive(Debug)]
-pub enum SudoExecutionOutput {
-    Success(SudoOuput),
-    WrongPassword,
-    Unauthorized,
-    ExecutionError(String),
-}
-
-impl SwitchedUserCommand {
-    /// Create new SwitchedUserCommand.
+impl SudoCommand {
+    /// Create new SudoCommand
     /// * `password` - The password used for `sudo`
-    /// * `command` - The command without arguments that will be launched
-    pub fn new<A: Display, B: Display>(password: A, program: B) -> SwitchedUserCommand {
-        SwitchedUserCommand {
+    /// * `program` - The command without arguments that will be launched
+    pub fn new<A: Display, B: Display>(password: A, program: B) -> SudoCommand {
+        SudoCommand {
             password: password.to_string(),
             program: program.to_string(),
             args: vec![],
         }
     }
 
-    /// Adds an argument to an exisiting SwitchedUserCommand
+    /// Adds an argument
     /// * `argument` - The argument that will be added
     pub fn arg<T: Display>(&mut self, argument: T) -> &mut Self {
         self.args = {
@@ -59,6 +56,7 @@ impl SwitchedUserCommand {
         self
     }
 
+    /// Adds multiple arguments
     pub fn args<T: Display>(&mut self, arguments: Vec<T>) -> &mut Self {
         for arg in arguments {
             self.arg(arg);
@@ -67,86 +65,29 @@ impl SwitchedUserCommand {
         self
     }
 
-    /// Spawns the SwitchedUserCommand with every added argument and the command.
-    /// The final `sudo` command looks like this:
-    /// `sudo -S -k COMMAND ARGS`
-    ///
-    /// The password is written to stdin without ANY checks if a password was required.
-    /// Normaly, sudo will ask for a password. If sudo does not ask for a password, the password
-    /// will still be written to stdin. Normaly, the input will just be ignored. If th command that
-    /// was passed does read from stdin though, the input will be read.
-    ///
-    /// As a Result the exist status (i32) will bre returned.
-    pub fn spawn(&self) -> SudoExecutionResult {
-        // Prepare vaiables
+    pub fn get_args(&self) -> Vec<String> {
+        self.args.clone()
+    }
+
+    /// Spawns a SudoCommand and captures the contents of standard input & standard error.
+    pub fn output(&self) -> Result<SudoOutput, SudoError> {
         let args = self.args.clone();
         let password = self.password.clone();
         let program = self.program.clone();
 
-        // Command Thread, handles the actual command
+        let prohibited_program = &[' ', '\n', '\r', '\t'];
+        let prohibited_password = &['\n', '\r'];
+        if program.chars().any(|x| prohibited_program.contains(&x))
+            || password.chars().any(|x| prohibited_password.contains(&x))
+        {
+            return Err(SudoError::BadParameters);
+        }
+
         let thread_handle = thread::spawn(move || {
             let mut command_handle = Command::new("sudo")
                 .arg("-S")
                 .arg("-k")
-                .args(program.clone().split(" "))
-                .args(args)
-                .stdin(Stdio::piped())
-                .stderr(Stdio::piped())
-                .stdout(Stdio::piped())
-                .spawn()
-                .expect("Failed to spawn process");
-
-            let password = password;
-            let mut stdinput = command_handle.stdin.take().unwrap();
-            let mut stderr = command_handle.stderr.take().unwrap();
-
-            stdinput
-                .write_all(format!("{password}\n").as_bytes())
-                .expect("Failed to write to stdin (password)");
-            std::thread::sleep(std::time::Duration::from_millis(500));
-            let after_password = &mut [0; 64];
-            let _ = stderr.read(after_password);
-            let after_password_string: String =
-                after_password.map(|x| (x as char).to_string()).join("");
-            if after_password_string.contains("Sorry, try again.") {
-                return SudoExecutionResult::WrongPassword;
-            }
-            if after_password_string.contains("is not in the sudoers file.") {
-                return SudoExecutionResult::Unauthorized;
-            }
-            match command_handle.wait().unwrap().code() {
-                Some(code) => SudoExecutionResult::Success(code),
-                None => SudoExecutionResult::Success(0),
-            }
-        });
-
-        thread_handle
-            .join()
-            .expect("Failed to join command thread with main thread")
-    }
-
-    #[allow(dead_code)]
-    /// Spawns the SwitchedUserCommand with every added argument and the command.
-    /// The final `sudo` command looks like this:
-    /// `sudo -S -k COMMAND ARGS`
-    ///
-    /// The password is written to stdin without ANY checks if a password was required.
-    /// Normaly, sudo will ask for a password. If sudo does not ask for a password, the password
-    /// will still be written to stdin. Normaly, the input will just be ignored. If th command that
-    /// was passed does read from stdin though, the input will be read.
-    ///
-    pub fn output(&self) -> SudoExecutionOutput {
-        // Prepare variables
-        let args = self.args.clone();
-        let password = self.password.clone();
-        let program = self.program.clone();
-
-        // Command Thread, handles the actual command
-        let thread_handle = thread::spawn(move || {
-            let mut command_handle = Command::new("sudo")
-                .arg("-S") // Read password from stdin
-                .arg("-k") // Force sudo to prompt for the password
-                .args(program.clone().split(" "))
+                .arg(program)
                 .args(args)
                 .stdin(Stdio::piped())
                 .stderr(Stdio::piped())
@@ -183,28 +124,24 @@ impl SwitchedUserCommand {
                 .read_to_string(&mut stderr_content)
                 .expect("Failed to read stderr");
 
-            // Check if sudo reported an incorrect password
-
-            if stderr_content.contains("incorrect password attempt")
-                || stderr_content.contains("Sorry, try again.")
-            {
-                return SudoExecutionOutput::WrongPassword;
+            if stderr_content.contains("Sorry, try again.") {
+                return Err(SudoError::WrongPassword);
             }
             if stderr_content.contains("is not in the sudoers file.") {
-                return SudoExecutionOutput::Unauthorized;
+                return Err(SudoError::NotInSudoers);
             }
 
             // Return the output if everything succeeded
-            SudoExecutionOutput::Success(SudoOuput {
+            Ok(SudoOutput {
                 stdout: stdout_content,
                 stderr: stderr_content,
+                status: command_handle.wait().unwrap().code(),
             })
         });
 
-        // Join the thread and return the result
-        thread_handle.join().unwrap_or_else(|_| {
-            SudoExecutionOutput::ExecutionError("Failed to join thread".to_string())
-        })
+        thread_handle
+            .join()
+            .unwrap_or(Err(SudoError::JoiningFailed))
     }
 }
 
@@ -212,7 +149,7 @@ pub fn verify_password(password: String) -> bool {
     let mut c = Command::new("sudo");
     c.args(["-S", "-k", "-v"]);
     c.stdin(Stdio::piped());
-    c.stdout(Stdio::piped());
+    c.stdout(Stdio::null());
     c.stderr(Stdio::piped());
     let mut h = c.spawn().unwrap();
     let mut stdin = h.stdin.take().unwrap();
@@ -226,8 +163,7 @@ pub fn verify_password(password: String) -> bool {
     e_reader
         .read_to_string(&mut stderr_content)
         .expect("Failed to read stdout");
-    !(stderr_content.contains("incorrect password attempt")
-        || stderr_content.contains("Sorry, try again."))
+    !stderr_content.contains("Sorry, try again.")
 }
 
 #[cfg(test)]
@@ -238,14 +174,11 @@ mod tests {
     fn authenticate_with_result() {
         let password =
             std::env::var("TEST_PASSWORD").expect("Requires TEST_PASSWORD environment variable");
-        let x = SwitchedUserCommand::new(password, "echo")
+        let x = SudoCommand::new(password, "echo")
             .args(vec!["tested"])
-            .spawn();
-        match x {
-            SudoExecutionResult::ExecutionError(e) => panic!("ExecutionError: {}, ", e),
-            SudoExecutionResult::WrongPassword => panic!("WrongPassword"),
-            SudoExecutionResult::Unauthorized => panic!("Unauthorized"),
-            _ => {}
+            .output();
+        if let Err(e) = x {
+            panic!("Failed with: {:?}", e);
         }
     }
 
@@ -253,14 +186,11 @@ mod tests {
     fn authenticate_with_output() {
         let password =
             std::env::var("TEST_PASSWORD").expect("Requires TEST_PASSWORD environment variable");
-        let x = SwitchedUserCommand::new(password, "echo")
+        let x = SudoCommand::new(password, "echo")
             .args(vec!["tested"])
             .output();
-        match x {
-            SudoExecutionOutput::ExecutionError(e) => panic!("ExecutionError: {}, ", e),
-            SudoExecutionOutput::WrongPassword => panic!("WrongPassword"),
-            SudoExecutionOutput::Unauthorized => panic!("Unauthorized"),
-            _ => {}
+        if let Err(e) = x {
+            panic!("Failed with: {:?}", e);
         }
     }
 }
